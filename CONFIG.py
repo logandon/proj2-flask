0177d9c0bfceeef027213552957c6f8804bc075b
logand6 <logand@uoregon.edu>
2015-10-09 15:34:49 -0700

    added changed config file

---
+++ CONFIG.py
@@ -0,0 +1,14 @@
+"""
+Configuration of syllabus server.
+Edit to fit development or deployment environment.
+
+"""
+import random 
+
+### My local development environment
+#SERVER_NAME = "localhost:5000"
+
+### On ix.cs.uoregon.edu
+SERVER_NAME = "ix.cs.uoregon.edu:{}".format(random.randint(5000,8000))
+PORT="{}".format(random.randint(5000,8000))
+DEBUG = False # Because it's unsafe to run outside localhost
